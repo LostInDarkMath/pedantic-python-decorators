4a9774034163cb77b3000f4949ffdbb89662c8fb
Willi <32729196+LostInDarkMath@users.noreply.github.com>
2020-08-11 16:10:06 +0200

    fix typo in travis.yaml and add long description for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
+from os import path
 
 url = "https://github.com/LostInDarkMath/PythonHelpers"
+author = "Willi Sontopski"
+
+# read the contents of the README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     # important things
@@ -18,11 +24,13 @@ setup(
     # },
 
     # metadata to display on PyPI
-    author="Willi Sontopski",
+    author=author,
     # author_email="me@example.com",
     license="Apache-2.0 License",
-    maintainer="Will Sontopski",
+    maintainer=author,
     description="Some useful Python decorators for cleaner software development.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords="decorators tools helpers type-checking pedantic type annotations",
     url=url,
     project_urls={
