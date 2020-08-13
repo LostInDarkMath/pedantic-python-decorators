8fdd1036c5698c56bc441bf0fd111d087a3f3d08
Willi <32729196+LostInDarkMath@users.noreply.github.com>
2020-08-13 19:17:00 +0200

    add .coveragerc to ignore venv in travis CI

--- get_coverage.bat
+++ get_coverage.bat
@@ -1,7 +1,7 @@
-@(
-echo [run]
-echo omit = *pedantic/unit_tests*
-) > .coveragerc
+rem @(
+rem echo [run]
+rem echo omit = *pedantic/unit_tests*
+rem ) > .coveragerc
 
 pip install coverage
 coverage run -m unittest pedantic/unit_tests/tests_main.py
