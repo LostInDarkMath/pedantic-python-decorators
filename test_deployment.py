a8cc40510b57d1d3aa99ab3ee8ce8a71c50a4d2d
LostInDarkMath <32729196+LostInDarkMath@users.noreply.github.com>
2020-09-08 14:48:40 +0200

    move check from after_success to script

--- test_deployment.py
+++ test_deployment.py
@@ -4,7 +4,7 @@ from pedantic import pedantic_class
 
 class AfterDeployment(unittest.TestCase):
     """Checks if the wheel was built correctly. That will fail if setup.py misses some files."""
-    def test_deployment_succesfull(self):
+    def test_deployment_successful(self):
         @pedantic_class
         class MyClass:
             def __init__(self):
