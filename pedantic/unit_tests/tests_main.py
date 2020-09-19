65540d716180ebf8738dec337624492f236306c4
LostInDarkMath <32729196+LostInDarkMath@users.noreply.github.com>
2020-09-19 22:08:02 +0200

    fix coveralls

--- pedantic/unit_tests/tests_main.py
+++ pedantic/unit_tests/tests_main.py
@@ -2,17 +2,16 @@ import unittest
 import sys
 import os
 
-if __name__ == '__main__':
-    sys.path.append(os.getcwd())
+sys.path.append(os.getcwd())
 
-    if sys.version_info >= (3, 7):
-        from pedantic.unit_tests.tests_docstring import TestRequireDocstringGoogleFormat
-        from pedantic.unit_tests.tests_pedantic_class_docstring import TestPedanticClassDocstring
+if sys.version_info >= (3, 7):
+    from pedantic.unit_tests.tests_docstring import TestRequireDocstringGoogleFormat
+    from pedantic.unit_tests.tests_pedantic_class_docstring import TestPedanticClassDocstring
 
-    from pedantic.unit_tests.tests_require_kwargs import TestRequireKwargs
-    from pedantic.unit_tests.tests_class_decorators import TestClassDecorators
-    from pedantic.unit_tests.tests_pedantic_class import TestPedanticClass
-    from pedantic.unit_tests.tests_pedantic import TestDecoratorRequireKwargsAndTypeCheck
-    from pedantic.unit_tests.tests_small_method_decorators import TestSmallDecoratorMethods
-    from pedantic.unit_tests.tests_combination_of_decorators import TestCombinationOfDecorators
-    unittest.main()
+from pedantic.unit_tests.tests_require_kwargs import TestRequireKwargs
+from pedantic.unit_tests.tests_class_decorators import TestClassDecorators
+from pedantic.unit_tests.tests_pedantic_class import TestPedanticClass
+from pedantic.unit_tests.tests_pedantic import TestDecoratorRequireKwargsAndTypeCheck
+from pedantic.unit_tests.tests_small_method_decorators import TestSmallDecoratorMethods
+from pedantic.unit_tests.tests_combination_of_decorators import TestCombinationOfDecorators
+unittest.main()
