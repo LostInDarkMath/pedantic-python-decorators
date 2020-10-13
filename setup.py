00ebd4972b3401c19c53265b8351f7f47bf7658f
Willi Sontopski <32729196+LostInDarkMath@users.noreply.github.com>
2020-10-13 17:39:30 +0200

    added support for typing.ForwardRef (#32)
    * added support for typing.ForwardRef
    
    * update documentation

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ author = "Willi Sontopski"
 
 setup(
     name="pedantic",
-    version="1.2.5",
+    version="1.2.6",
     python_requires='>=3.6.1',
     packages=find_packages(),
     install_requires=['docstring_parser'],
