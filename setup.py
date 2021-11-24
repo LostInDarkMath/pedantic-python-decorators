b644789cc38ef4c08edec6b23ef5d73096734707
LostInDarkMath <willi_sontopski@arcor.de>
2021-11-24 14:06:29 +0100

    default values are not validated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ author = "Willi Sontopski"
 
 setup(
     name="pedantic",
-    version="1.6.0",
+    version="1.6.1",
     python_requires='>=3.6.1',
     packages=find_packages(),
     install_requires=['docstring_parser'],
