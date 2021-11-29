a1d0fd61c861bdf33db2c35ed1851af348a33b44
LostInDarkMath <willi_sontopski@arcor.de>
2021-11-29 17:22:30 +0100

    validate decorator now supports async flask endpoints

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ author = "Willi Sontopski"
 
 setup(
     name="pedantic",
-    version="1.6.3",
+    version="1.7.0",
     python_requires='>=3.6.1',
     packages=find_packages(),
     install_requires=['docstring_parser'],
