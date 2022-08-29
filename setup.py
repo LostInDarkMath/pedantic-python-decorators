f08fdb48203a0edec1bb868b307069add10d1998
Willi Sontopski <willi.sontopski@peerox.de>
2022-08-29 15:26:09 +0200

    release 1.11.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ author = "Willi Sontopski"
 
 setup(
     name="pedantic",
-    version="1.11.3",
+    version="1.11.4",
     python_requires='>=3.7.0',
     packages=find_packages(),
     install_requires=[],
