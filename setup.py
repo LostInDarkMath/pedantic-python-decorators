a09e370b739e3a9706e765e6f883f1d6b2c808d9
LostInDarkMath <willi_sontopski@arcor.de>
2022-10-26 12:35:46 +0200

    increase version number to 1.13.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ author = "Willi Sontopski"
 
 setup(
     name="pedantic",
-    version="1.13.1",
+    version="1.13.2",
     python_requires='>=3.7.0',
     packages=find_packages(),
     install_requires=[],
